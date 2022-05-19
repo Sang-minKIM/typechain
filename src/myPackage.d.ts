cd97eb24f8850da14bc4f5a08699d45510e4f74d
Sang-minKIM <87116017+Sang-minKIM@users.noreply.github.com>
2022-05-19 15:31:17 +0900

    JSDoc

--- src/myPackage.d.ts
+++
@@ -1,8 +0,0 @@
-interface Config {
-    url:string
-}
-declare module "myPackage" {
-    function init(config:Config):boolean;
-    function exit(code:number):number;
-}
-// 정의파일 작성하기. 정의파일에는 구현을 할 필요는 없고 타입만 정해주면 된다. 
--- src/myPackage.js
+++ src/myPackage.js
@@ -1,6 +1,22 @@
+//@ts-check
+/**
+ * Initializes the project
+ * @param {object} config
+ * @param {boolean} config.debug
+ * @param {string} config.url 
+ * @returns {boolean}
+ */
 export function init(config){
     return true
 }
+
+/**
+ * Exit the program
+ * @param {number} code 
+ * @returns {number}
+ */
 export function exit(code){
     return code +1;
 }
+/*프로젝트 안에 자바스크립트와 타입스크립트 파일이 같이 들어있는 경우, 자바스크립트에서 타입스크립트로 이전하는 경우를 많이 맞딱드릴 것이다. 
+이 떄 JSDoc을 사용하면 파일은 JS그대로 두고 TS의 보호만 받을 수 있다.*/
