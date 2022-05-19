a5b077dfaf35c1abfb9459f3a79b26cc260ad48d
Sang-minKIM <87116017+Sang-minKIM@users.noreply.github.com>
2022-05-19 14:35:28 +0900

    declaration file

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,7 @@
+import {exit, init} from "myPackage";
 
 
-class Block {
-    constructor(
-        private data: string
-    ){}
-    static hello(){
-        return "hi"
-    }
-}
+init({
+    url:"true"
+})
+exit(1)
